0297a8dcd75e39a22576bd0b7a6e11ecd67a704a
MorganeBeCode <morgane.jansen@gmail.com>
2019-11-07 14:30:14 +0100

    add favicon

--- home.php
+++ home.php
@@ -14,6 +14,9 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--Favicon-->
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <!--Stylesheets-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
--- login.php
+++ login.php
@@ -47,6 +47,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--Favicon-->
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <!--Stylesheets-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
